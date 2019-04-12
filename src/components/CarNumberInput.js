6dc4d2886a3c228d66f9b874aad504a85ed6d0e0
Ivan Biletskyi <ivbiletskiy@gmail.com>
2019-04-12 13:49:18 +0300

    readme updated

--- src/components/CarNumberInput.js
+++ src/components/CarNumberInput.js
@@ -8,73 +8,67 @@ import { setCarNumberInputError } from "../redux/actions/rootActions";
 const UKRAINIAN_CAR_NUMBER_REGEXP = /^[АВЕІКМНОРСТХABEIKMHOPCTX]{2}[0-9]{4}[АВЕІКМНОРСТХABEIKMHOPCTX]{2}$/; // АВЕІКМНОРСТХABEIKMHOPCTX - it is not twice copied string, it is cyrillic and latin letters
 
 class CarNumberInput extends Component {
-    static propTypes = {
-        searchCar: PropTypes.func,
-        setCarNumberInputError: PropTypes.func
-    };
-    state = {
-        inputValue: "",
-        isInputError: false
-    };
+  static propTypes = {
+    searchCar: PropTypes.func,
+    setCarNumberInputError: PropTypes.func,
+  };
 
-    handleInputValueChange = event => {
-        this.setState({ inputValue: event.target.value, isInputError: false });
-    };
+  state = {
+    inputValue: "",
+    isInputError: false,
+  };
 
-    handleSubmit = event => {
-        event.preventDefault();
-        this.setState(
-            { inputValue: this.state.inputValue.toUpperCase() },
-            () => {
-                if (this.isCorrectCarNumber(this.state.inputValue)) {
-                    this.props.searchCar(this.state.inputValue);
-                } else {
-                    this.setState({ isInputError: true });
-                }
-            }
-        );
-    };
-
-    isCorrectCarNumber = carNumber => {
-        return UKRAINIAN_CAR_NUMBER_REGEXP.test(carNumber);
-    };
-
-    shouldComponentUpdate(_nextProps, nextState) {
-        //we don't want to spam Redux store every time, when user input a character into the input field, so we write there only if error flag toggled
-        if (nextState.isInputError !== this.state.isInputError) {
-            this.props.setCarNumberInputError(nextState.isInputError);
-        }
-        return true;
+  handleInputValueChange = event => {
+    //we don't want to spam Redux store every time, when user input a character into the input field, so we write there only if internal error flag toggled
+    if (this.state.isInputError) {
+      this.props.setCarNumberInputError(false);
     }
+    this.setState({ inputValue: event.target.value, isInputError: false });
+  };
 
-    render() {
-        return (
-            <header className="header">
-                <form onSubmit={this.handleSubmit} className="search-form">
-                    <label>
-                        <h3 className="search-form__title">
-                            Проверка авто по номерному знаку:
-                        </h3>
-                        <input
-                            className="search-form__input"
-                            type="text"
-                            value={this.state.inputValue}
-                            onChange={this.handleInputValueChange}
-                            placeholder="Номерной знак авто"
-                        />
-                    </label>
-                </form>
-                <span className="header__error-text">
-                    {this.state.isInputError &&
-                        "Введите корректный номерной знак"}
-                </span>
-            </header>
-        );
-    }
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({ inputValue: this.state.inputValue.toUpperCase() }, () => {
+      if (this.isCorrectCarNumber(this.state.inputValue)) {
+        this.props.searchCar(this.state.inputValue);
+      } else {
+        this.setState({ isInputError: true });
+        this.props.setCarNumberInputError(true);
+      }
+    });
+  };
+
+  isCorrectCarNumber = carNumber => {
+    return UKRAINIAN_CAR_NUMBER_REGEXP.test(carNumber);
+  };
+
+  render() {
+    return (
+      <header className="header">
+        <form onSubmit={this.handleSubmit} className="search-form">
+          <label>
+            <h3 className="search-form__title">
+              Проверка авто по номерному знаку:
+            </h3>
+            <input
+              className="search-form__input"
+              type="text"
+              value={this.state.inputValue}
+              onChange={this.handleInputValueChange}
+              placeholder="Номерной знак авто"
+            />
+          </label>
+        </form>
+        <span className="header__error-text">
+          {this.state.isInputError && "Введите корректный номерной знак"}
+        </span>
+      </header>
+    );
+  }
 }
 const mapDispatchToProps = { searchCar, setCarNumberInputError };
 
 export default connect(
-    null,
-    mapDispatchToProps
+  null,
+  mapDispatchToProps
 )(CarNumberInput);
